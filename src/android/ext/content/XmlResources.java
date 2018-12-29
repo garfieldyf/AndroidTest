b31aedfba028502137fffc4668b03e382a873b03
garfieldyf <yuanfengdiy@sina.com>
2018-12-29 16:33:24 +0800

    删除 DrawableBinder 类。

--- src/android/ext/content/XmlResources.java
+++ src/android/ext/content/XmlResources.java
@@ -14,7 +14,6 @@ import android.ext.content.image.BitmapDecoder.Parameters;
 import android.ext.content.image.BitmapDecoder.PercentParameters;
 import android.ext.content.image.BitmapDecoder.ScaleParameters;
 import android.ext.content.image.BitmapDecoder.SizeParameters;
-import android.ext.content.image.DrawableBinder;
 import android.ext.content.image.ImageBinder;
 import android.ext.content.image.ImageBinder.BitmapTransformer;
 import android.ext.content.image.ImageBinder.DrawableTransformer;
@@ -187,9 +186,6 @@ public final class XmlResources {
             switch (name) {
             case "ImageBinder":
                 return new ImageBinder(context, attrs);
-
-            case "DrawableBinder":
-                return new DrawableBinder(context, attrs);
 
             case "TransitionBinder":
                 return new TransitionBinder(context, attrs);

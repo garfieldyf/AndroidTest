ffe1fd6dfe839e637142ccaa5818b0a4354af369
garfieldyf <yuanfengdiy@sina.com>
2019-06-05 12:15:37 +0800

    删除 RecyclerPageAdapter.java

--- src/android/ext/page/PageAdapter.java
+++ src/android/ext/page/PageAdapter.java
@@ -1,21 +1,37 @@
 package android.ext.page;
 
+import java.util.BitSet;
+import java.util.Formatter;
+import java.util.Map.Entry;
+import java.util.Set;
 import android.ext.cache.Cache;
-import android.ext.page.Pages.PageAdapterImpl;
-import android.ext.page.Pages.PageLoader;
 import android.ext.util.DebugUtils;
+import android.ext.util.UIHandler;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.Printer;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 
 /**
  * Class <tt>PageAdapter</tt> allows to loading data by page.
- * @param E The item data type of the adapter.
+ * <h3>PageAdapter's generic types</h3>
+ * <p>The two types used by a page adapter are the following:</p>
+ * <ol><li><tt>E</tt>, The item data type of the adapter.</li>
+ * <li><tt>VH</tt>, A class that extends {@link ViewHolder} that
+ * will be used by the adapter.</li></ol>
  * @author Garfield
  */
-public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E> {
-    private final PageAdapterImpl<E> mImpl;
+@SuppressWarnings("unchecked")
+public abstract class PageAdapter<E, VH extends ViewHolder> extends Adapter<VH> {
+    private int mItemCount;
+    private final int mPageSize;
+    private final int mInitialSize;
+    private final int mPrefetchDistance;
+
+    private final BitSet mPageStates;
+    private RecyclerView mRecyclerView;
+    private final Cache<Integer, Page<E>> mPageCache;
 
     /**
      * Constructor
@@ -44,69 +60,129 @@ public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E
      * @see #PageAdapter(int, int, int, int)
      */
     public PageAdapter(Cache<Integer, ? extends Page<? extends E>> pageCache, int initialSize, int pageSize, int prefetchDistance) {
-        mImpl = new PageAdapterImpl<E>(pageCache, initialSize, pageSize, prefetchDistance, this);
+        DebugUtils.__checkError(pageSize <= 0 || initialSize <= 0, "pageSize <= 0 || initialSize <= 0");
+        DebugUtils.__checkError(prefetchDistance > Math.min(pageSize, initialSize), "prefetchDistance = " + prefetchDistance + " greater than pageSize = " + Math.min(pageSize, initialSize));
+        mInitialSize = initialSize;
+        mPageSize    = pageSize;
+        mPageStates  = new BitSet();
+        mPageCache   = (Cache<Integer, Page<E>>)pageCache;
+        mPrefetchDistance = prefetchDistance;
+    }
+
+    /**
+     * @see #setItemCount(int)
+     */
+    @Override
+    public int getItemCount() {
+        return mItemCount;
     }
 
     /**
      * Sets total number of items in this adapter.
      * @param count The total number of items in this adapter.
-     * @see #getCount()
+     * @see #getItemCount()
      */
-    public void setCount(int count) {
-        mImpl.setItemCount(count);
-        if (count > 0) {
-            notifyDataSetChanged();
-        } else {
-            notifyDataSetInvalidated();
+    public void setItemCount(int count) {
+        DebugUtils.__checkUIThread("setItemCount");
+        DebugUtils.__checkError(count < 0, "count < 0");
+        DebugUtils.__checkError(mRecyclerView == null, "This adapter not attached to RecyclerView.");
+        if (mItemCount != count) {
+            mItemCount = count;
+            mPageCache.clear();
+            mPageStates.clear();
+            UIHandler.notifyDataSetChanged(mRecyclerView);
         }
-    }
-
-    /**
-     * @see #setCount(int)
-     */
-    @Override
-    public int getCount() {
-        return mImpl.mItemCount;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return -1;
-    }
-
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        if (view == null) {
-            view = newView(position, parent);
-        }
-
-        bindView(getItem(position), position, view);
-        return view;
     }
 
     /**
      * Returns the item associated with the specified position <em>position</em> in this adapter.
      * <p>This method will be call {@link #loadPage(int, int, int)} to obtain the item when the
      * item was not present.</p>
-     * @param position The adapter position of the item.
+     * @param position The adapter position of the item in this adapter.
      * @return The item at the specified position, or <tt>null</tt> if there was not present.
-     * @see #peekItem(int)
+     * @see #getItem(View)
+     * @see #getItem(ViewHolder)
      */
-    @Override
     public E getItem(int position) {
-        return mImpl.getItem(position);
+        DebugUtils.__checkUIThread("getItem");
+        DebugUtils.__checkError(position < 0 || position >= mItemCount, "Index out of bounds - position = " + position + ", itemCount = " + mItemCount);
+        final long combinedPosition = getPageForPosition(position);
+        final int page = Pages.getOriginalPage(combinedPosition);
+        final Page<E> result = getPage(page);
+        if (mPrefetchDistance > 0) {
+            prefetchPage(page, (int)combinedPosition, position, mPrefetchDistance);
+        }
+
+        return (result != null ? result.getItem((int)combinedPosition) : null);
+    }
+
+    /**
+     * Equivalent to calling <tt>getItem(recyclerView.getChildAdapterPosition(view))</tt>.
+     * @param child The child of the <tt>RecyclerView</tt> to query for the
+     * <tt>ViewHolder</tt>'s adapter position.
+     * @return The item at the specified position, or <tt>null</tt> if there was not present.
+     * @see #getItem(int)
+     * @see #getItem(ViewHolder)
+     */
+    public final E getItem(View child) {
+        DebugUtils.__checkError(mRecyclerView == null, "This adapter not attached to RecyclerView.");
+        final int position = mRecyclerView.getChildAdapterPosition(child);
+        return (position != RecyclerView.NO_POSITION ? getItem(position) : null);
+    }
+
+    /**
+     * Equivalent to calling <tt>getItem(viewHolder.getAdapterPosition())</tt>.
+     * @param viewHolder The {@link ViewHolder} to query its adapter position.
+     * @return The item at the specified position, or <tt>null</tt> if there was not present.
+     * @see #getItem(int)
+     * @see #getItem(View)
+     */
+    public final E getItem(ViewHolder viewHolder) {
+        final int position = viewHolder.getAdapterPosition();
+        return (position != RecyclerView.NO_POSITION ? getItem(position) : null);
     }
 
     /**
      * Returns the item associated with the specified position <em>position</em> in this adapter.
      * <p>Unlike {@link #getItem}, this method do <b>not</b> call {@link #loadPage(int, int, int)}
      * when the item was not present.</p>
-     * @param position The adapter position of the item.
+     * @param position The adapter position of the item in this adapter.
      * @return The item at the specified position, or <tt>null</tt> if there was not present.
-     * @see #getItem(int)
+     * @see #peekItem(View)
+     * @see #peekItem(ViewHolder)
      */
     public E peekItem(int position) {
-        return mImpl.peekItem(position);
+        DebugUtils.__checkUIThread("peekItem");
+        DebugUtils.__checkError(position < 0 || position >= mItemCount, "Index out of bounds - position = " + position + ", itemCount = " + mItemCount);
+        final long combinedPosition = getPageForPosition(position);
+        final Page<E> result = mPageCache.get(Pages.getOriginalPage(combinedPosition));
+        return (result != null ? result.getItem((int)combinedPosition) : null);
+    }
+
+    /**
+     * Equivalent to calling <tt>peekItem(recyclerView.getChildAdapterPosition(view))</tt>.
+     * @param child The child of the <tt>RecyclerView</tt> to query for the
+     * <tt>ViewHolder</tt>'s adapter position.
+     * @return The item at the specified position, or <tt>null</tt> if there was not present.
+     * @see #peekItem(int)
+     * @see #peekItem(ViewHolder)
+     */
+    public final E peekItem(View child) {
+        DebugUtils.__checkError(mRecyclerView == null, "This adapter not attached to RecyclerView.");
+        final int position = mRecyclerView.getChildAdapterPosition(child);
+        return (position != RecyclerView.NO_POSITION ? peekItem(position) : null);
+    }
+
+    /**
+     * Equivalent to calling <tt>peekItem(viewHolder.getAdapterPosition())</tt>.
+     * @param viewHolder The {@link ViewHolder} to query its adapter position.
+     * @return The item at the specified position, or <tt>null</tt> if there was not present.
+     * @see #peekItem(int)
+     * @see #peekItem(View)
+     */
+    public final E peekItem(ViewHolder viewHolder) {
+        final int position = viewHolder.getAdapterPosition();
+        return (position != RecyclerView.NO_POSITION ? peekItem(position) : null);
     }
 
     /**
@@ -118,7 +194,26 @@ public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E
      * @see #peekPage(int)
      */
     public Page<E> getPage(int page) {
-        return mImpl.getPage(page);
+        DebugUtils.__checkUIThread("getPage");
+        DebugUtils.__checkError(page < 0, "page < 0");
+        Page<E> result = mPageCache.get(page);
+        if (result == null && !mPageStates.get(page)) {
+            // Computes the startPosition and itemCount to load.
+            final int startPosition, itemCount;
+            if (page == 0) {
+                itemCount = mInitialSize;
+                startPosition = 0;
+            } else {
+                itemCount = mPageSize;
+                startPosition = (page - 1) * mPageSize + mInitialSize;
+            }
+
+            // Loads the page data and marks the page loading state.
+            mPageStates.set(page);
+            loadPage(page, startPosition, itemCount);
+        }
+
+        return result;
     }
 
     /**
@@ -131,30 +226,85 @@ public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E
      */
     public Page<E> peekPage(int page) {
         DebugUtils.__checkUIThread("peekPage");
-        return mImpl.mPageCache.get(page);
+        return mPageCache.get(page);
+    }
+
+    /**
+     * Equivalent to calling <tt>setPage(page, data, null)</tt>.
+     * @param page The index of the page.
+     * @param data May be <tt>null</tt>. The <tt>Page</tt> to add.
+     * @see #setPage(int, Page, Object)
+     * @see Pages#newPage(java.util.List)
+     */
+    public final void setPage(int page, Page<? extends E> data) {
+        setPage(page, data, null);
     }
 
     /**
      * Sets the {@link Page} at the specified index <em>page</em> in this adapter.
-     * <p>This is useful when asynchronously loading to prevent blocking the UI.</p>
+     * This method will be call {@link #notifyItemRangeChanged(int, int, Object)}
+     * when the <em>page</em> has added. <p>This is useful when asynchronously
+     * loading to prevent blocking the UI.</p>
      * @param page The index of the page.
      * @param data May be <tt>null</tt>. The <tt>Page</tt> to set.
+     * @param payload Optional parameter, pass to {@link #notifyItemRangeChanged}.
+     * @see #setPage(int, Page)
      * @see Pages#newPage(java.util.List)
      */
-    public void setPage(int page, Page<? extends E> data) {
-        if (mImpl.setPage(page, data) > 0) {
-            notifyDataSetChanged();
+    public void setPage(int page, Page<? extends E> data, Object payload) {
+        DebugUtils.__checkUIThread("setPage");
+        DebugUtils.__checkError(page < 0, "page < 0");
+        DebugUtils.__checkError(mRecyclerView == null, "This adapter not attached to RecyclerView.");
+
+        // Clears the page loading state when the page is load complete.
+        mPageStates.clear(page);
+        final int itemCount = Pages.getCount(data);
+        if (itemCount > 0) {
+            mPageCache.put(page, (Page<E>)data);
+            UIHandler.notifyItemRangeChanged(mRecyclerView, getPositionForPage(page, 0), itemCount, payload);
         }
     }
 
     /**
-     * Returns the item count of the specified index <em>page</em>.
+     * Adds the {@link Page} at the specified index <em>page</em> in this adapter.
+     * This method will be call {@link #notifyItemRangeInserted(int, int)} when
+     * the <em>page</em> has added. <p>This is useful when asynchronously loading
+     * to prevent blocking the UI.</p>
+     * @param page The index of the page.
+     * @param data May be <tt>null</tt>. The <tt>Page</tt> to add.
+     * @see Pages#newPage(java.util.List)
+     */
+    public void addPage(int page, Page<? extends E> data) {
+        DebugUtils.__checkUIThread("addPage");
+        DebugUtils.__checkError(mRecyclerView == null, "This adapter not attached to RecyclerView.");
+
+        // Clears the page loading state when the page is load complete.
+        mPageStates.clear(page);
+        final int itemCount = Pages.getCount(data);
+        if (itemCount > 0) {
+            mItemCount += itemCount;
+            mPageCache.put(page, (Page<E>)data);
+            UIHandler.notifyItemRangeInserted(mRecyclerView, getPositionForPage(page, 0), itemCount);
+        }
+    }
+
+    /**
+     * Returns the {@link RecyclerView} associated with this adapter.
+     * @return The {@link RecyclerView} object or <tt>null</tt> if
+     * this adapter not attached to the <tt>RecyclerView</tt>.
+     */
+    public final RecyclerView getRecyclerView() {
+        return mRecyclerView;
+    }
+
+    /**
+     * Returns the item count in the specified page.
      * @param page The index of the page.
      * @return The item count.
      */
     public final int getPageSize(int page) {
         DebugUtils.__checkError(page < 0, "page < 0");
-        return (page > 0 ? mImpl.mPageSize : mImpl.mInitialSize);
+        return (page > 0 ? mPageSize : mInitialSize);
     }
 
     /**
@@ -169,7 +319,9 @@ public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E
      * @see Pages#getOriginalPosition(long)
      */
     public final long getPageForPosition(int position) {
-        return mImpl.getPageForPosition(position);
+        DebugUtils.__checkError(position < 0, "position < 0");
+        final int offset = position - mInitialSize;
+        return (offset < 0 ? (position & 0xFFFFFFFFL) : (((long)(offset / mPageSize + 1) << 32) | ((offset % mPageSize) & 0xFFFFFFFFL)));
     }
 
     /**
@@ -180,41 +332,64 @@ public abstract class PageAdapter<E> extends BaseAdapter implements PageLoader<E
      * @see #getPageForPosition(int)
      */
     public final int getPositionForPage(int page, int position) {
-        return mImpl.getPositionForPage(page, position);
+        DebugUtils.__checkError(page < 0 || position < 0, "page < 0 || position < 0");
+        return (page > 0 ? (page - 1) * mPageSize + mInitialSize + position : position);
     }
 
+    @SuppressWarnings("resource")
     public final void dump(Printer printer) {
-        mImpl.dump(printer, getClass().getSimpleName());
+        DebugUtils.__checkUIThread("dump");
+        final StringBuilder result = new StringBuilder(128);
+        final Formatter formatter  = new Formatter(result);
+        final Set<Entry<Integer, Page<E>>> entries = mPageCache.entries().entrySet();
+
+        DebugUtils.dumpSummary(printer, result, 100, " Dumping %s [ initialSize = %d, pageSize = %d, itemCount = %d ] ", getClass().getSimpleName(), mInitialSize, mPageSize, mItemCount);
+        result.setLength(0);
+        printer.println(DebugUtils.toString(mPageCache, result.append("  PageCache [ ")).append(", size = ").append(entries.size()).append(" ]").toString());
+
+        for (Entry<Integer, Page<E>> entry : entries) {
+            final Page<E> page = entry.getValue();
+            result.setLength(0);
+
+            formatter.format("    Page %-2d ==> ", entry.getKey());
+            printer.println(DebugUtils.toString(page, result).append(" { count = ").append(page.getCount()).append(" }").toString());
+        }
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
     }
 
     /**
-     * Returns a new {@link View} to hold the item data.
-     * @param position The adapter position of the item.
-     * @param parent The parent to which the new view is attached to.
-     * @return The newly created view.
-     * @see #bindView(E, int, View)
-     */
-    protected abstract View newView(int position, ViewGroup parent);
-
-    /**
-     * Binds an existing {@link View} to hold the item data.
-     * @param itemData The item data to bind view or <tt>null</tt>.
-     * @param position The adapter position of the item.
-     * @param view Existing view, returned earlier by {@link #newView}.
-     * @see #newView(int, ViewGroup)
-     */
-    protected abstract void bindView(E itemData, int position, View view);
-
-    /**
-     * Returns the {@link Page} at the given position <em>page</em>. Subclasses
-     * must implement this method to return <tt>Page</tt> for a particular page.
-     * <p>If you want to asynchronously load the page data to prevent blocking
-     * the UI, it is possible to return <tt>null</tt> and at a later time call
-     * {@link #setPage(int, Page)}.<p>
-     * @param page The index of the page whose data should be returned.
+     * Loads the {@link Page} at the given page index <em>page</em>. Subclasses
+     * must implement this method to load the data for a particular page. <p>If
+     * you want to asynchronously load the page data to prevent blocking the UI,
+     * it is possible to load the data on a background thread and at a later time
+     * call {@link #setPage(int, Page, Object)}.<p>
+     * @param page The index of the page whose data should be load.
      * @param startPosition The position of the first item to load.
-     * @param count The number of items to load.
-     * @return The <tt>Page</tt>, or <tt>null</tt>.
+     * @param itemCount The number of items to load.
      */
-    public abstract Page<E> loadPage(int page, int startPosition, int count);
+    protected abstract void loadPage(int page, int startPosition, int itemCount);
+
+    /**
+     * Prefetch the {@link Page} with the given <em>page</em> and <em>position</em>.
+     * The default implementation prefetch the next page data from the current page.
+     * @param page The index of the current page.
+     * @param position The index of the item in the <em>page</em>.
+     * @param adapterPosition The adapter position of the item in this adapter.
+     * @param prefetchDistance Defines how far to the first or last item in the page.
+     */
+    protected void prefetchPage(int page, int position, int adapterPosition, int prefetchDistance) {
+        final int lastPage = (mItemCount - mInitialSize - 1) / mPageSize + 1;
+        if (page < lastPage) {
+            // Prefetch the next page data.
+            final int pageSize = (page > 0 ? mPageSize : mInitialSize);
+            if (position == pageSize - prefetchDistance) {
+                DebugUtils.__checkDebug(true, "PageAdapter", "prefetchPage = " + (page + 1) + ", position = " + position);
+                getPage(page + 1);
+            }
+        }
+    }
 }
--- src/android/ext/page/Pages.java
+++ src/android/ext/page/Pages.java
@@ -1,10 +1,6 @@
 package android.ext.page;
 
-import java.util.BitSet;
-import java.util.Formatter;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Set;
 import org.json.JSONArray;
 import android.database.Cursor;
 import android.ext.cache.ArrayMapCache;
@@ -14,7 +10,6 @@ import android.ext.database.DatabaseUtils;
 import android.ext.util.ArrayUtils;
 import android.ext.util.DebugUtils;
 import android.ext.util.JsonUtils;
-import android.util.Printer;
 
 /**
  * Class Pages
@@ -306,160 +301,6 @@ public final class Pages {
         protected void entryRemoved(boolean evicted, Integer key, ResourcePage<E> oldPage, ResourcePage<E> newPage) {
             if (evicted || oldPage != newPage) {
                 oldPage.close();
-            }
-        }
-    }
-
-    /**
-     * A <tt>PageLoader</tt> used to load page data.
-     */
-    /* package */ static interface PageLoader<E> {
-        Page<E> loadPage(int page, int startPosition, int count);
-    }
-
-    /**
-     * Class <tt>PageAdapterImpl</tt> is an implementation of a page adapter.
-     */
-    /* package */ static final class PageAdapterImpl<E> {
-        /* package */ int mItemCount;
-        /* package */ final int mPageSize;
-        /* package */ final int mInitialSize;
-        /* package */ final int mPrefetchDistance;
-
-        /* package */ final BitSet mPageStates;
-        /* package */ final PageLoader<E> mPageLoader;
-        /* package */ final Cache<Integer, Page<E>> mPageCache;
-
-        @SuppressWarnings("unchecked")
-        /* package */ PageAdapterImpl(Cache<Integer, ? extends Page<? extends E>> pageCache, int initialSize, int pageSize, int prefetchDistance, PageLoader<E> loader) {
-            DebugUtils.__checkError(pageSize <= 0 || initialSize <= 0, "pageSize <= 0 || initialSize <= 0");
-            DebugUtils.__checkError(prefetchDistance > Math.min(pageSize, initialSize), "prefetchDistance = " + prefetchDistance + " greater than pageSize = " + Math.min(pageSize, initialSize));
-            mInitialSize = initialSize;
-            mPageLoader  = loader;
-            mPageSize    = pageSize;
-            mPageStates  = new BitSet();
-            mPageCache   = (Cache<Integer, Page<E>>)pageCache;
-            mPrefetchDistance = prefetchDistance;
-        }
-
-        /* package */ final void setItemCount(int count) {
-            DebugUtils.__checkError(count < 0, "count < 0");
-            mItemCount = count;
-            mPageCache.clear();
-            mPageStates.clear();
-        }
-
-        /* package */ final E getItem(int position) {
-            DebugUtils.__checkUIThread("getItem");
-            DebugUtils.__checkError(position < 0 || position >= mItemCount, "Index out of bounds - position = " + position + ", itemCount = " + mItemCount);
-            final long combinedPosition = getPageForPosition(position);
-            final int currentPage = getOriginalPage(combinedPosition);
-            final Page<E> page = getPage(currentPage);
-            if (mPrefetchDistance > 0) {
-                prefetchPage(currentPage, (int)combinedPosition);
-            }
-
-            return (page != null ? page.getItem((int)combinedPosition) : null);
-        }
-
-        /* package */ final E peekItem(int position) {
-            DebugUtils.__checkUIThread("peekItem");
-            DebugUtils.__checkError(position < 0 || position >= mItemCount, "Index out of bounds - position = " + position + ", itemCount = " + mItemCount);
-            final long combinedPosition = getPageForPosition(position);
-            final Page<E> page = mPageCache.get(getOriginalPage(combinedPosition));
-            return (page != null ? page.getItem((int)combinedPosition) : null);
-        }
-
-        /* package */ final Page<E> getPage(int page) {
-            DebugUtils.__checkUIThread("getPage");
-            DebugUtils.__checkError(page < 0, "page < 0");
-            Page<E> result = mPageCache.get(page);
-            if (result == null && !mPageStates.get(page)) {
-                // Computes the startPosition and count to load.
-                final int startPosition, count;
-                if (page == 0) {
-                    count = mInitialSize;
-                    startPosition = 0;
-                } else {
-                    count = mPageSize;
-                    startPosition = (page - 1) * mPageSize + mInitialSize;
-                }
-
-                // Loads the page data and marks the page loading state.
-                mPageStates.set(page);
-                result = mPageLoader.loadPage(page, startPosition, count);
-                if (getCount(result) > 0) {
-                    // If the page is load successful.
-                    // 1. Adds the page to page cache.
-                    // 2. Clears the page loading state.
-                    mPageStates.clear(page);
-                    mPageCache.put(page, result);
-                }
-            }
-
-            return result;
-        }
-
-        @SuppressWarnings("unchecked")
-        /* package */ final int setPage(int page, Page<? extends E> data) {
-            DebugUtils.__checkUIThread("setPage");
-            DebugUtils.__checkError(page < 0, "page < 0");
-
-            // Clears the page loading state when the page is load complete.
-            mPageStates.clear(page);
-            final int count = getCount(data);
-            if (count > 0) {
-                mPageCache.put(page, (Page<E>)data);
-            }
-
-            return count;
-        }
-
-        /* package */ final long getPageForPosition(int position) {
-            DebugUtils.__checkError(position < 0, "position < 0");
-            final int offset = position - mInitialSize;
-            return (offset < 0 ? (position & 0xFFFFFFFFL) : (((long)(offset / mPageSize + 1) << 32) | ((offset % mPageSize) & 0xFFFFFFFFL)));
-        }
-
-        /* package */ final int getPositionForPage(int page, int position) {
-            DebugUtils.__checkError(page < 0 || position < 0, "page < 0 || position < 0");
-            return (page > 0 ? (page - 1) * mPageSize + mInitialSize + position : position);
-        }
-
-        @SuppressWarnings("resource")
-        /* package */ final void dump(Printer printer, String className) {
-            final StringBuilder result = new StringBuilder(128);
-            final Formatter formatter  = new Formatter(result);
-            final Set<Entry<Integer, Page<E>>> entries = mPageCache.entries().entrySet();
-
-            DebugUtils.dumpSummary(printer, result, 100, " Dumping %s [ initialSize = %d, pageSize = %d, itemCount = %d ] ", className, mInitialSize, mPageSize, mItemCount);
-            result.setLength(0);
-            printer.println(DebugUtils.toString(mPageCache, result.append("  PageCache [ ")).append(", size = ").append(entries.size()).append(" ]").toString());
-
-            for (Entry<Integer, Page<E>> entry : entries) {
-                final Page<E> page = entry.getValue();
-                result.setLength(0);
-
-                formatter.format("    Page %-2d ==> ", entry.getKey());
-                printer.println(DebugUtils.toString(page, result).append(" { count = ").append(page.getCount()).append(" }").toString());
-            }
-        }
-
-        private void prefetchPage(int currentPage, int position) {
-            // Prefetch the previous page data.
-            if (currentPage > 0 && position == mPrefetchDistance - 1) {
-                DebugUtils.__checkDebug(true, "Pages", "prefetchPage = " + (currentPage - 1) + ", position = " + position);
-                getPage(currentPage - 1);
-            }
-
-            final int lastPage = (mItemCount - mInitialSize - 1) / mPageSize + 1;
-            if (currentPage < lastPage) {
-                // Prefetch the next page data.
-                final int pageSize = (currentPage > 0 ? mPageSize : mInitialSize);
-                if (position == pageSize - mPrefetchDistance) {
-                    DebugUtils.__checkDebug(true, "Pages", "prefetchPage = " + (currentPage + 1) + ", position = " + position);
-                    getPage(currentPage + 1);
-                }
             }
         }
     }

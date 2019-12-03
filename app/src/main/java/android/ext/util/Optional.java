3d21904413f3fc3347e3d2b464331af6bad66968
garfieldyf <yuanfengdiy@sina.com>
2019-12-03 15:44:08 +0800

    修改 Optional 实现

--- app/src/main/java/android/ext/util/Optional.java
+++ app/src/main/java/android/ext/util/Optional.java
@@ -11,7 +11,7 @@ import android.ext.cache.Caches;
 public final class Optional {
     /**
      * Returns a {@link Cancelable} from the specified <em>cancelable</em>,
-     * if non-null. Otherwise returns an empty {@code Cancelable}.
+     * if non-null. Otherwise returns an empty <tt>Cancelable</tt>.
      */
     public static Cancelable ofNullable(Cancelable cancelable) {
         return (cancelable != null ? cancelable : EmptyCancelable.sInstance);
@@ -19,7 +19,7 @@ public final class Optional {
 
     /**
      * Returns a {@link Cache} from the specified <em>cache</em>,
-     * if non-null. Otherwise returns an empty {@code Cache}.
+     * if non-null. Otherwise returns an empty <tt>Cache</tt>.
      * @see Caches#emptyCache()
      */
     public static <K, V> Cache<K, V> ofNullable(Cache<K, V> cache) {
@@ -28,7 +28,7 @@ public final class Optional {
 
     /**
      * Returns a {@link BitmapPool} from the specified <em>bitmapPool</em>,
-     * if non-null. Otherwise returns an empty {@code BitmapPool}.
+     * if non-null. Otherwise returns an empty <tt>BitmapPool</tt>.
      * @see Caches#emptyBitmapPool()
      */
     public static BitmapPool ofNullable(BitmapPool bitmapPool) {
@@ -51,6 +51,94 @@ public final class Optional {
             return false;
         }
     }
+
+//    /**
+//     * Class <tt>EmptyDrawable</tt> is an implementation of a {@link Drawable}.
+//     */
+//    private static final class EmptyDrawable extends Drawable {
+//        public static final Drawable sInstance = new EmptyDrawable();
+//
+//        @Override
+//        public int getOpacity() {
+//            return PixelFormat.TRANSPARENT;
+//        }
+//
+//        @Override
+//        public void draw(Canvas canvas) {
+//        }
+//
+//        @Override
+//        public int getMinimumWidth() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public int getMinimumHeight() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public void setAlpha(int alpha) {
+//        }
+//
+//        @Override
+//        public boolean setState(int[] stateSet) {
+//            return false;
+//        }
+//
+//        @Override
+//        public void invalidateSelf() {
+//        }
+//
+//        @Override
+//        public void unscheduleSelf(Runnable what) {
+//        }
+//
+//        @Override
+//        public void scheduleSelf(Runnable what, long when) {
+//        }
+//
+//        @Override
+//        public void setBounds(Rect bounds) {
+//        }
+//
+//        @Override
+//        public void setBounds(int left, int top, int right, int bottom) {
+//        }
+//
+//        @Override
+//        public void setTint(int tint) {
+//        }
+//
+//        @Override
+//        public void setTintList(ColorStateList tint) {
+//        }
+//
+//        @Override
+//        public void clearColorFilter() {
+//        }
+//
+//        @Override
+//        public void setColorFilter(ColorFilter filter) {
+//        }
+//
+//        @Override
+//        public void setColorFilter(int color, Mode mode) {
+//        }
+//
+//        @Override
+//        public boolean setVisible(boolean visible, boolean restart) {
+//            return false;
+//        }
+//
+//        @Override
+//        public void inflate(Resources res, XmlPullParser parser, AttributeSet attrs) {
+//        }
+//
+//        @Override
+//        public void inflate(Resources res, XmlPullParser parser, AttributeSet attrs, Theme theme) {
+//        }
+//    }
 
     /**
      * This utility class cannot be instantiated.

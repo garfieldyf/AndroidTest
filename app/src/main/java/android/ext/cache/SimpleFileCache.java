956e4df5a289a3ce215fdd8d3b98c236baacce32
garfieldyf <yuanfengdiy@sina.com>
2020-01-18 16:57:04 +0800

    修改 SimpleFileCache 实现

--- app/src/main/java/android/ext/cache/SimpleFileCache.java
+++ app/src/main/java/android/ext/cache/SimpleFileCache.java
@@ -1,34 +1,25 @@
 package android.ext.cache;
 
-import static android.ext.util.FileUtils.FLAG_IGNORE_HIDDEN_FILE;
-import static android.ext.util.FileUtils.FLAG_SCAN_FOR_DESCENDENTS;
 import android.content.Context;
 import android.ext.util.ArrayUtils;
 import android.ext.util.DebugUtils;
 import android.ext.util.FileUtils;
-import android.ext.util.FileUtils.Dirent;
-import android.ext.util.FileUtils.ScanCallback;
 import android.os.Process;
 import android.util.Printer;
 import java.io.File;
-import java.util.Arrays;
 
 /**
  * Class <tt>SimpleFileCache</tt> is an implementation of a {@link FileCache}.
- * By default, this cache size is the number of entries. Overrides {@link #sizeOf}
- * to size this cache in different units.
  * @author Garfield
  */
-public class SimpleFileCache implements FileCache, ScanCallback {
+public class SimpleFileCache implements FileCache {
     private final int mMaxSize;
     private final File mCacheDir;
 
     /**
      * Constructor
      * @param cacheDir The absolute path of the cache directory.
-     * @param maxSize For caches that do not override {@link #sizeOf}, this is
-     * the maximum number of files to allow in this cache. For all other caches,
-     * this is the maximum sum of the sizes of the files to allow in this cache.
+     * @param maxSize The maximum number of files to allow in this cache.
      * @see #SimpleFileCache(Context, String, int)
      */
     public SimpleFileCache(File cacheDir, int maxSize) {
@@ -42,9 +33,7 @@ public class SimpleFileCache implements FileCache, ScanCallback {
      * Constructor
      * @param context The <tt>Context</tt>.
      * @param name A relative path within the cache directory, such as <tt>"file_cache"</tt>.
-     * @param maxSize For caches that do not override {@link #sizeOf}, this is the maximum
-     * number of files to allow in this cache. For all other caches, this is the maximum sum
-     * of the sizes of the files to allow in this cache.
+     * @param maxSize The maximum number of files to allow in this cache.
      * @see #SimpleFileCache(File, int)
      */
     public SimpleFileCache(Context context, String name, int maxSize) {
@@ -63,8 +52,8 @@ public class SimpleFileCache implements FileCache, ScanCallback {
     }
 
     /**
-     * Returns the maximum size in this cache in user-defined units.
-     * @return The maximum size.
+     * Returns the maximum number of files allow in this cache.
+     * @return The maximum number of files.
      */
     public final int maxSize() {
         return mMaxSize;
@@ -75,7 +64,13 @@ public class SimpleFileCache implements FileCache, ScanCallback {
      * @return The total number of bytes.
      */
     public final long getCacheSize() {
-        return FileUtils.computeFileBytes(mCacheDir);
+        long result = 0;
+        final String[] names = mCacheDir.list();
+        for (int i = ArrayUtils.getSize(names) - 1; i >= 0; --i) {
+            result += new File(mCacheDir, names[i]).length();
+        }
+
+        return result;
     }
 
     /**
@@ -88,9 +83,12 @@ public class SimpleFileCache implements FileCache, ScanCallback {
         try {
             DebugUtils.__checkStartMethodTracing();
             Process.setThreadPriority(Process.THREAD_PRIORITY_FOREGROUND);
-            final MutableInt size = new MutableInt();
-            FileUtils.scanFiles(mCacheDir.getPath(), this, FLAG_IGNORE_HIDDEN_FILE | FLAG_SCAN_FOR_DESCENDENTS, size);
-            DebugUtils.__checkStopMethodTracing("SimpleFileCache", "trimToSize size = " + size.value + ", maxSize = " + mMaxSize);
+            final String[] names = mCacheDir.list();
+            final int size = ArrayUtils.getSize(names);
+            for (int i = mMaxSize; i < size; ++i) {
+                new File(mCacheDir, names[i]).delete();
+            }
+            DebugUtils.__checkStopMethodTracing("SimpleFileCache", "trimToSize size = " + size + ", maxSize = " + mMaxSize + (size > mMaxSize ? ", delete file count = " + (size - mMaxSize) : ""));
         } finally {
             Process.setThreadPriority(priority);
         }
@@ -104,7 +102,7 @@ public class SimpleFileCache implements FileCache, ScanCallback {
     @Override
     public File get(String key) {
         DebugUtils.__checkError(key == null, "key == null");
-        return new File(mCacheDir, new StringBuilder(key.length() + 3).append('/').append(key.charAt(0)).append('/').append(key).toString());
+        return new File(mCacheDir, key);
     }
 
     @Override
@@ -114,88 +112,22 @@ public class SimpleFileCache implements FileCache, ScanCallback {
 
     @Override
     public File remove(String key) {
-        final File cacheFile = get(key);
+        final File cacheFile = new File(mCacheDir, key);
         return (cacheFile.delete() ? cacheFile : null);
     }
 
-    @Override
-    public int onScanFile(String path, int type, Object cookie) {
-        if (type == Dirent.DT_REG) {
-            final MutableInt size = (MutableInt)cookie;
-            if (size.value < mMaxSize) {
-                final int result = sizeOf(path);
-                DebugUtils.__checkError(result < 0, "Negative size: " + path);
-                size.value += result;
-            } else {
-                FileUtils.deleteFiles(path, false);
-                DebugUtils.__checkDebug(true, "SimpleFileCache", "delete cache file - " + path);
-            }
-        }
-
-        return SC_CONTINUE;
-    }
-
-    /**
-     * Returns the size of the cache file for <em>cacheFile</em> in user-defined
-     * units. The default implementation returns 1 so that size is the number of
-     * files and max size is the maximum number of files.
-     * @param cacheFile The cache file.
-     * @return The size of the cache file, must be <tt>>= 0</tt>.
-     */
-    protected int sizeOf(String cacheFile) {
-        return 1;
-    }
-
     /* package */ final void dump(Printer printer) {
-        final StringBuilder result = new StringBuilder(256);
-        final File[] files = mCacheDir.listFiles();
-        final int size = ArrayUtils.getSize(files);
+        final String[] names = mCacheDir.list();
+        final int size = ArrayUtils.getSize(names);
+
+        long length = 0;
+        for (int i = 0; i < size; ++i) {
+            length += new File(mCacheDir, names[i]).length();
+        }
+
+        final StringBuilder result = new StringBuilder(100);
+        DebugUtils.dumpSummary(printer, result, 100, " Dumping SimpleFileCache [ files = %d, size = %s ] ", size, FileUtils.formatFileSize(length));
         result.setLength(0);
-        if (size > 0) {
-            Arrays.sort(files);
-        }
-
-        long fileCounts = 0, fileLengths = 0;
-        final long[] results  = new long[2];
-        for (int i = 0, index = 0; i < size; ++i) {
-            final File file = files[i];
-            if (file.isDirectory()) {
-                ++index;
-                getFileInfo(file, results);
-                result.append("  ").append(file.getName()).append(" { files = ").append(results[0]).append(", size = ").append(FileUtils.formatFileSize(results[1])).append(" }");
-
-                fileCounts  += results[0];
-                fileLengths += results[1];
-            }
-
-            if ((index % 4) == 0) {
-                result.append('\n');
-            }
-        }
-
-        DebugUtils.dumpSummary(printer, new StringBuilder(130), 130, " Dumping SimpleFileCache [ dirs = %d, files = %d, size = %s ] ", size, fileCounts, FileUtils.formatFileSize(fileLengths));
-        if (result.length() > 0) {
-            printer.println(result.toString());
-        }
-    }
-
-    private static void getFileInfo(File directory, long[] outResults) {
-        final File[] files  = directory.listFiles();
-        final int fileCount = ArrayUtils.getSize(files);
-
-        long fileLength = 0;
-        for (int i = 0; i < fileCount; ++i) {
-            fileLength += files[i].length();
-        }
-
-        outResults[0] = fileCount;
-        outResults[1] = fileLength;
-    }
-
-    /**
-     * Class <tt>MutableInt</tt> wrapper for the primitive type <tt>int</tt>.
-     */
-    /* package */ static final class MutableInt {
-        public int value;
+        printer.println(result.append("  cacheDir = ").append(mCacheDir.getPath()).toString());
     }
 }

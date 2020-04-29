6fdb9035632831002575d34c29c131d8d7d3f5a7
garfieldyf <yuanfengdiy@sina.com>
2020-04-29 13:14:26 +0800

    修改 main.cpp，增加调试信息

--- app/src/main/java/android/ext/image/ImageModule.java
+++ app/src/main/java/android/ext/image/ImageModule.java
@@ -41,7 +41,6 @@ import android.util.Printer;
 import android.util.SparseArray;
 import android.util.TypedValue;
 import android.util.Xml;
-import java.nio.ByteOrder;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -89,7 +88,6 @@ public final class ImageModule<URI, Image> implements ComponentCallbacks2, Facto
      * @param fileCache May be <tt>null</tt>. The {@link FileCache} to store the loaded image files.
      */
     /* package */ ImageModule(Context context, Executor executor, Cache<URI, Image> imageCache, FileCache fileCache) {
-        DebugUtils.__checkDebug(true, "JNI_OnLoad", "java byteOrder = " + ByteOrder.nativeOrder());
         final int maxPoolSize = computeBufferPoolMaxSize(executor);
         mContext  = context.getApplicationContext();
         mExecutor = executor;
--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -29,6 +29,21 @@
 #include "procutils.h"
 #endif
 
+#ifndef NDEBUG
+__STATIC_INLINE__ jstring getByteOrder(JNIEnv* env)
+{
+    assert(env);
+
+    jclass clazz = env->FindClass("java/nio/ByteOrder");
+    assert(clazz != NULL);
+
+    jobject byteOrder = env->CallStaticObjectMethod(clazz, env->GetStaticMethodID(clazz, "nativeOrder", "()Ljava/nio/ByteOrder;"));
+    assert(byteOrder != NULL);
+
+    return (jstring)env->CallObjectMethod(byteOrder, env->GetMethodID(clazz, "toString", "()Ljava/lang/String;"));
+}
+#endif  // NDEBUG
+
 __BEGIN_DECLS
 ///////////////////////////////////////////////////////////////////////////////
 // The VM calls JNI_OnLoad when the native library is loaded.
@@ -41,9 +56,10 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* /*reserved*/)
 
 #ifndef NDEBUG
     const uint32_t num = 0x12345678;
+    JNI::jstring_t jbyteOrder(env, getByteOrder(env));
     const char* byteOrders[] = { "BIG_ENDIAN", "LITTLE_ENDIAN" };
     const uint32_t index = (*((const char*)&num) == 0x12 ? 0 : 1);
-    LOGD("java version = 0x%08x, sdk = %d, sizeof(void*) = %zu, sizeof(int) = %zu, sizeof(long) = %zu, sizeof(Color) = %zu, native byteOrder = %s\n", env->GetVersion(), ::android_get_device_api_level(), sizeof(void*), sizeof(int), sizeof(long), sizeof(__NS::Color), byteOrders[index]);
+    LOGD("JNI version = 0x%08x, sdk = %d, sizeof(void*) = %zu, sizeof(int) = %zu, sizeof(long) = %zu, sizeof(Color) = %zu, java byteOrder = %s, native byteOrder = %s\n", env->GetVersion(), ::android_get_device_api_level(), sizeof(void*), sizeof(int), sizeof(long), sizeof(__NS::Color), jbyteOrder.str(), byteOrders[index]);
 #endif  // NDEBUG
 
 #ifdef __BUILD_GIFIMAGE__

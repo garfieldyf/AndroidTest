7d4f81fc457aa448aabece936adb4b4764613d96
garfieldyf <yuanfengdiy@sina.com>
2020-07-03 18:33:49 +0800

    修改编译脚本

--- ant/build.bat
+++ ant/build.bat
@@ -1,3 +1,3 @@
 echo off
-rem ant compile-all -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dcompile_barcode=true -Dcompile_foucs=true
-ant compile-java -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dcompile_barcode=true -Dcompile_foucs=true
+rem ant compile-all -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dmax_pool_size=64 -Dcompile_barcode=true -Dcompile_foucs=true
+ant compile-java -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dmax_pool_size=64 -Dcompile_barcode=true -Dcompile_foucs=true

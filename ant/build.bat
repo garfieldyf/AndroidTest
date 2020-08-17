7d329081b46cfd08fd1e52360ab64bea543dbe67
garfieldyf <yuanfengdiy@sina.com>
2020-08-17 12:00:58 +0800

    修改 build 脚本，添加 androidx 支持

--- ant/build.bat
+++ ant/build.bat
@@ -1,3 +1,3 @@
 echo off
-rem ant compile-all -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dmax_pool_size=64 -Dcompile_barcode=true -Dcompile_foucs=true
-ant compile-java -Dmodule.name=androidext -Djni.package=android/ext -Djava.package=android.ext -Dmax_pool_size=64 -Dcompile_barcode=true -Dcompile_foucs=true
+rem ant compile-all -Dmodule.name=androidext -Djni.package=com/android/ext -Djava.package=com.android.ext -Dmax_pool_size=32 -Dcompile_barcode=true -Dcompile_foucs=true -Dandroidx_support=true
+ant compile-java -Dmodule.name=androidext -Djni.package=com/android/ext -Djava.package=com.android.ext -Dmax_pool_size=32 -Dcompile_barcode=false -Dcompile_foucs=false -Dandroidx_support=true

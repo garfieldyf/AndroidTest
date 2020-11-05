1494759c3f23025a5a97e82c96e0e4a0948944d5
garfieldyf <yuanfengdiy@sina.com>
2020-11-05 15:53:54 +0800

    修改 ContactPhotoDecoder 实现

--- app/src/main/java/android/ext/image/decoder/ContactPhotoDecoder.java
+++ app/src/main/java/android/ext/image/decoder/ContactPhotoDecoder.java
@@ -1,7 +1,5 @@
 package android.ext.image.decoder;
 
-import android.content.ContentResolver;
-import android.ext.database.DatabaseUtils;
 import android.ext.image.ImageModule;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,13 +10,9 @@ import java.io.InputStream;
 
 /**
  * Class <tt>ContactPhotoDecoder</tt> used to decode the contact photo data to a {@link Bitmap}.
- * @param <Image> Must be <tt>Bitmap</tt> or <tt>Object</tt> that will be decode
- * the result type.
  * @author Garfield
  */
-public class ContactPhotoDecoder<Image> extends BitmapDecoder<Image> {
-    private static final ThreadLocal<byte[]> sPhotoLocal = new ThreadLocal<byte[]>();
-
+public final class ContactPhotoDecoder extends BitmapDecoder<Bitmap> {
     /**
      * Constructor
      * @param module The {@link ImageModule}.
@@ -28,56 +22,10 @@ public class ContactPhotoDecoder<Image> extends BitmapDecoder<Image> {
     }
 
     @Override
-    protected Bitmap decodeBitmap(Object uri, Options opts) {
-        final ContentResolver resolver = mModule.mContext.getContentResolver();
-        final Uri contactUri = (Uri)uri;
-        if (opts.inJustDecodeBounds) {
-            decodeContactPhoto(resolver, contactUri, opts);
-            if (opts.outWidth <= 0) {
-                sPhotoLocal.remove();   // Clear the contact photo data.
-                decodePhotoBounds(resolver, contactUri, opts);
-            }
-        } else {
-            Bitmap photo = decodeContactPhoto(resolver, contactUri, opts);
-            if (photo != null) {
-                return photo;
-            }
-
-            // Decode the contact's thumbnail photo.
-            final byte[] data = sPhotoLocal.get();
-            if (data != null && (photo = BitmapFactory.decodeByteArray(data, 0, data.length, opts)) != null) {
-                sPhotoLocal.remove();   // Clear the contact photo data.
-                return photo;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Decode the contact's thumbnail photo bounds (including width, height, mimeType etc).
-     */
-    private static void decodePhotoBounds(ContentResolver resolver, Uri contactUri, Options opts) {
-        final Uri photoUri = Uri.withAppendedPath(contactUri, Photo.CONTENT_DIRECTORY);
-        final byte[] data = DatabaseUtils.simpleQueryBlob(resolver, photoUri, Photo.PHOTO, null, null);
-        if (data != null) {
-            BitmapFactory.decodeByteArray(data, 0, data.length, opts);
-            if (opts.outWidth > 0) {
-                // Save the contact photo data.
-                sPhotoLocal.set(data);
-            }
-        }
-    }
-
-    /**
-     * Decode the contact's display photo.
-     */
-    private static Bitmap decodeContactPhoto(ContentResolver resolver, Uri contactUri, Options opts) {
-        final Uri photoUri = Uri.withAppendedPath(contactUri, Photo.DISPLAY_PHOTO);
-        try (final InputStream is = resolver.openInputStream(photoUri)) {
+    protected Bitmap decodeBitmap(Object uri, Options opts) throws Exception {
+        final Uri photoUri = Uri.withAppendedPath((Uri)uri, Photo.DISPLAY_PHOTO);
+        try (final InputStream is = mModule.mContext.getContentResolver().openInputStream(photoUri)) {
             return BitmapFactory.decodeStream(is, null, opts);
-        } catch (Exception e) {
-            return null;
         }
     }
 }

05993f027a606019f1ae07d14f66aceacaaae127
garfieldyf <yuanfengdiy@sina.com>
2020-11-20 13:32:04 +0800

    修改 Algorithms 实现

--- app/src/main/java/com/tencent/test/Algorithms.java
+++ app/src/main/java/com/tencent/test/Algorithms.java
@@ -15,6 +15,54 @@ public final class Algorithms {
         public Node next;
     }
 
+    ////////////////////////////////// LinkedList //////////////////////////////////
+
+    private static Node make() {
+        Node head = null;
+        for (int i = 0; i < 6; ++i) {
+            Node node  = new Node();
+            node.value = NUMBERS[i];
+            node.next  = head;
+            head = node;
+        }
+
+        return head;
+    }
+
+    private static void print(Node head) {
+        StringBuilder s = new StringBuilder();
+        while (head != null) {
+            s.append(head.value).append(' ');
+            head = head.next;
+        }
+
+        Log.d("abcd", "print = " + s.toString());
+    }
+
+    private static Node reverse(Node head) {
+        Node newHead = null;
+        while (head != null) {
+            final Node node = head;
+            head = head.next;
+            node.next = newHead;
+            newHead = node;
+        }
+
+        return newHead;
+    }
+
+    public static void reverse() {
+        final Node head = make();
+        // 7 1 41 5 8 0
+        print(head);
+
+        final Node newHead = reverse(head);
+        // 0 8 5 41 1 7
+        print(newHead);
+    }
+
+    ////////////////////////////////// CircularLinkedList //////////////////////////////////
+
     private static Node makeCircular() {
         Node head = null, last = null;
         for (int i = 0; i < 6; ++i) {
@@ -46,41 +94,7 @@ public final class Algorithms {
         Log.d("abcd", "printCircular = " + s.toString());
     }
 
-    private static Node make() {
-        Node head = null;
-        for (int i = 0; i < 6; ++i) {
-            Node node  = new Node();
-            node.value = NUMBERS[i];
-            node.next  = head;
-            head = node;
-        }
-
-        return head;
-    }
-
-    private static void print(Node head) {
-        StringBuilder s = new StringBuilder();
-        while (head != null) {
-            s.append(head.value).append(' ');
-            head = head.next;
-        }
-
-        Log.d("abcd", "print = " + s.toString());
-    }
-
-    public static Node reverse(Node head) {
-        Node newHead = null;
-        while (head != null) {
-            final Node node = head;
-            head = head.next;
-            node.next = newHead;
-            newHead = node;
-        }
-
-        return newHead;
-    }
-
-    public static Node reverseCircular(Node head) {
+    private static Node reverseCircular(Node head) {
         if (head == null) {
             return null;
         }
@@ -98,16 +112,6 @@ public final class Algorithms {
         return newHead;
     }
 
-    public static void reverse() {
-        final Node head = make();
-        // 7 1 41 5 8 0
-        print(head);
-
-        final Node newHead = reverse(head);
-        // 0 8 5 41 1 7
-        print(newHead);
-    }
-
     public static void reverseCircular() {
         final Node head = makeCircular();
         // 7 1 41 5 8 0
@@ -117,6 +121,8 @@ public final class Algorithms {
         // 0 8 5 41 1 7
         printCircular(newHead);
     }
+
+    ////////////////////////////////// binarySearch //////////////////////////////////
 
     public static int binarySearch(int key) {
         final int[] numbers = NUMBERS.clone();
@@ -139,6 +145,8 @@ public final class Algorithms {
         return -1;
     }
 
+    ////////////////////////////////// remove //////////////////////////////////
+
     public static void remove() {
         // 5, 41, 0, 60, 43, -1, 967
         final int[] indexes = { 1, 6, 8, 4, 0, 11 };
@@ -156,6 +164,8 @@ public final class Algorithms {
 
         System.out.println(numbers);
     }
+
+    ////////////////////////////////// maxRange //////////////////////////////////
 
     public static void maxRange(int w) {
         if (w < 1 || w > NUMBERS.length) {
@@ -186,9 +196,12 @@ public final class Algorithms {
         System.out.println(Arrays.toString(result));
     }
 
-    private static final String STRING = "fjjkkkdsaaaaab"; // f2j3kds5ab  fjkdsab
+    ////////////////////////////////// unique //////////////////////////////////
+
+    private static final String STRING = "fjjkkkdsaaaaab";
 
     public static void unique() {
+        // fjkdsab
         final int length = STRING.length();
         if (length <= 1) {
             System.out.println(STRING);
@@ -208,7 +221,10 @@ public final class Algorithms {
         System.out.println(s);
     }
 
+    ////////////////////////////////// compress //////////////////////////////////
+
     public static void compress() {
+        // f2j3kds5ab
         final int length = STRING.length();
         if (length <= 1) {
             System.out.println(STRING);
@@ -241,5 +257,93 @@ public final class Algorithms {
         }
 
         System.out.println(s);
+    }
+
+    ////////////////////////////////// big integer add //////////////////////////////////
+
+    private static final int[] BIG_INTEGER1 = { 7, 5, 3, 4, 6, 1, 9, 4, 3 };
+    private static final int[] BIG_INTEGER2 = { 3, 7, 9, 0, 2, 1, 8 };
+
+    private static Node create(int[] numbers) {
+        Node head = null;
+        for (int i = 0; i < numbers.length; ++i) {
+            Node node  = new Node();
+            node.value = numbers[i];
+            node.next  = head;
+            head = node;
+        }
+
+        return head;
+    }
+
+    private static void printList(Node head) {
+        StringBuilder s = new StringBuilder();
+        while (head != null) {
+            s.append(head.value);
+            head = head.next;
+        }
+
+        Log.d("abcd", "printList = " + s.toString());
+    }
+
+    public static void add() {
+        /*
+         *  349164357
+         *          +
+         *    8120973
+         * -----------
+         *  357285330
+         *
+         *    9164357
+         *          +
+         *    8120973
+         * -----------
+         *   17285330
+         */
+        Node head1 = create(BIG_INTEGER1);
+        Node head2 = create(BIG_INTEGER2);
+        printList(head1);
+        printList(head2);
+
+        Node rhead1 = reverse(head1);
+        Node rhead2 = reverse(head2);
+
+        int carry = 0;
+        Node newHead = null;
+        while (rhead1 != null || rhead2 != null) {
+            int value1 = 0;
+            if (rhead1 != null) {
+                value1 = rhead1.value;
+                rhead1 = rhead1.next;
+            }
+
+            int value2 = 0;
+            if (rhead2 != null) {
+                value2 = rhead2.value;
+                rhead2 = rhead2.next;
+            }
+
+            int result = value1 + value2 + carry;
+            if (result >= 10) {
+                carry = 1;
+                result -= 10;
+            } else {
+                carry = 0;
+            }
+
+            Node node  = new Node();
+            node.value = result;
+            node.next  = newHead;
+            newHead = node;
+        }
+
+        if (carry != 0) {
+            Node node  = new Node();
+            node.value = carry;
+            node.next  = newHead;
+            newHead = node;
+        }
+
+        printList(newHead);
     }
 }
